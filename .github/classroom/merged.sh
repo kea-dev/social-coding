1718816169757bbfb93a8596186f66c5c20cfe80
Simon Grønborg <simon.gronborg@gmail.com>
2023-02-14 15:11:04 +0100

    Update merged.sh

--- .github/classroom/merged.sh
+++ .github/classroom/merged.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+#!/bin/bash 
+set -e
 
 echo Looking up if all branches are merged into master
 
-Merged:
+# Merged:
 
+echo "Testing if father is merged to master"
 git branch --merged master  | grep -P "father"
+
+echo "Testing if lion is merged to master"
 git branch --merged master  | grep -P "lion"
+
+echo "Testing if turtle is merged to master"
 git branch --merged master  | grep -P "turtle"
 
-Not Merge:
+# Not Merge:
 
+echo "Testing if there are unmerged branches to master"
 git branch --no-merged master
